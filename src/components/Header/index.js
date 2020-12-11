88d2ce67075097438941c67b1f3701ab62712dd2
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-11 18:12:12 -0300

    refactor: wrap header elements on a container

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,11 +6,13 @@ import './styles.css';
 function Header() {
   return(
     <header>
-      <h1>heyloh</h1>
-      <div className="links">
-        <Link to="#Home">Home</Link>
-        <Link to="#Skills">Skills</Link>
-        <Link to="#Projects">Projetos</Link>
+      <div className="container">
+        <h1>heyloh</h1>
+        <div className="links">
+          <Link to="#Home">Home</Link>
+          <Link to="#Skills">Skills</Link>
+          <Link to="#Projects">Projetos</Link>
+        </div>
       </div>
     </header>
   );
